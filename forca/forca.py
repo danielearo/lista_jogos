ddaf9b9da6326b9bd632b42626d2837240652cc8
Dani <daniele.aro@aluno.senai.br>
2026-05-18 11:15:11 -0300

    funções para criar o jogo da forca

--- forca/forca.py
+++ forca/forca.py
@@ -22,8 +22,10 @@ def escolher_palavra() -> str: #str significa que só pode texto
 
 #print(palavra_escolhida)
 
-def desenhar_forca(erro):
+def desenhar_forca(erro:int):
     """Imprima o desenho da forca dependendo da quantidade de erros"""
+    limpar()
+
     if erro == 0:
         print(r"""
           _____
@@ -34,7 +36,7 @@ def desenhar_forca(erro):
           |
           |
           """)
-    elif erro ==1:
+    elif erro == 1:
         print (r"""
           _____
           |   |
@@ -66,7 +68,7 @@ def desenhar_forca(erro):
           |
           """)
    
-    elif erro == 5:
+    elif erro == 4:
         print (r"""
           _____
           |   |
@@ -77,7 +79,7 @@ def desenhar_forca(erro):
           |  /
           """)
         
-    elif erro == 6:
+    elif erro == 5:
         print (r"""
           _____
           |   |
@@ -88,3 +90,40 @@ def desenhar_forca(erro):
           |  / \
           """)
     
+#desenhar_forca(5) - somente para testar se está correto
+
+def gerar_tracos(palavra: str) -> list:
+    """Gera e retorna uma lista contendo underlines (_) na mesma quantidade que as letras da palavra"""
+    quantidade_de_letras = len(palavra)  #len significa a quantidade letras;
+    tracos = []
+
+    #usando while e contador
+    contador = 0
+    while contador < quantidade_de_letras:
+        tracos.append("_")
+        contador = contador + 1
+
+    #outra forma, comparando a quantidade de itens de _ na lista traços;
+    #while len(tracos) < quantidade_de_letras:
+        #tracos.append("_")
+
+    #outra forma, usando for
+    #for X in palavra:
+        #tracos.append("_")
+
+    return tracos
+
+# lista_tracos = gerar_tracos("CASA")
+# print(*lista_tracos)
+
+def perguntar_letra() -> str:
+    #pergunta uma letra
+    resposta = input("Escolha uma letra:").upper() #letra maiuscula
+    
+    while len(resposta) !=1:
+        resposta = input("Eu disse apenas UMA letra: ").upper()
+        return resposta
+    
+letra = perguntar_letra()
+print(letra)
+
